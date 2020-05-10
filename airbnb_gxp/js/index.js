82ad393bf65a45d92e59da939cf3f6e3060cbaec
sealexsandro <sealexsandro@gmail.com>
2020-05-10 11:02:25 -0300

    melhorias no script js

--- airbnb_gxp/js/index.js
+++ airbnb_gxp/js/index.js
@@ -3,10 +3,8 @@ const cardsQuartos = document.querySelector("#cardsQuartos");
 dados = [];
 
 async function fecthCardsQuartos() {
-    return await fetch(url_api).then(async (r) => await r.json());
+    return await fetch(url_api).then(async (r) => r.json());
 }
-
-
 
 function renderCards(dataCards) {
     cardsQuartos.innerHTML = " ";
@@ -33,27 +31,6 @@ function moldarCard(card){
     cardsQuartos.appendChild(div);
 
 }
-// function renderCard(card) {
-//     const div = document.createElement("div");
-//     div.style.width = "20rem";
-//     div.style.margin = "2rem";
-//     div.className = "card";
-//     div.classList.add('card');
-//     div.innerHTML = `
-//     <img src= "${card.photo}" class="card-img-top" alt="" />
-//     <div class= "card-body">
-//         <h5 class= "card-title">${card.name}</h5>
-//         <p class = "card-text">
-//             Tipo: ${card.property_type}
-//         </p>
-//         <p class = "card-text">
-//             Preço: ${card.price},00
-//         </p>
-//     </div>
-//     `;
-//     cardsQuartos.appendChild(div);
-// }
-
 async function main() {
     dados = await fecthCardsQuartos();
     if (dados[0]) {
